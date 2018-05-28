0a5510db9f9ba4c879a69c5fc8889bb5a2ef03ac
StaffanBetner <staffan@betner.nu>
2018-05-28 09:59:46 -0400

    Initial commit

---
+++ server.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(tidyverse)
+calc_prob <- function(cM){(exp(-3.5+0.64*cM)/(1+exp(-3.5+0.64*cM))) %>% round(2)}
+
+shinyServer(function(input, output, session) {
+
+  inFile <- reactive({
+    if (is.null(input$file)) {
+      return(NULL)
+    } else {
+      input$file
+    }
+  })
+  
+  myData <- reactive({
+    if (is.null(inFile())) {
+      return(NULL)
+    } else {
+      out <- inFile()$datapath %>% read_csv %>% 
+        unique() %>% 
+        mutate(new_cm = CENTIMORGANS*calc_prob(CENTIMORGANS)) %>% 
+        group_by(MATCHNAME) %>% 
+        summarise(`UNWEIGHTED SUM OF CENTIMORGANS` = sum(CENTIMORGANS) %>% round(2),
+                  `REWEIGHTED SUM OF CENTIMORGANS` = (sum(new_cm)-max(new_cm)+max(CENTIMORGANS)) %>% round(2),
+                  `LONGEST SEGMENT` = max(CENTIMORGANS) %>% round(2)) %>% ungroup %>% arrange(desc(`REWEIGHTED SUM OF CENTIMORGANS`))
+      out}
+  })
+  
+  observe({
+    output$table <- renderDataTable({ if (is.null(inFile())) {
+      return(NULL)
+    } else {myData()}})})
+  
+})
---
+++ ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+shinyUI(
+  navbarPage(
+    "Reweighter of Family Finder Matches List",
+    tabPanel(
+      "Tool",
+      sidebarPanel(
+        fileInput('file', 'Upload one CSV File with Chromosome Browser Results (Max. Size 50 MB)',
+                  accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'), multiple = F)
+      ),
+      mainPanel(
+        dataTableOutput("table")
+      )),
+    tabPanel("About",
+             mainPanel(helpText("Chromium based (e.g. Chrome or Opera) browsers are recommended, Microsoft Edge won't work.", br(), 
+                                "The uploaded files will only be stored temporary and will be deleted after the session is closed.", br(),
+                                tags$p("Source available",tags$a(href = "https://github.com/StaffanBetner/overlappingsegments", "here.")),br(),
+                                "Contact: staffan@betner.nu or @StaffanBetner (Twitter)")))
+  ))
