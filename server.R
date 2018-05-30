76a74f0c4986fa1bcacb219529c3f9ff8e3049e1
StaffanBetner <staffan@betner.nu>
2018-05-30 16:27:45 -0400

    Updates on the algorithm and the output

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(tidyverse)
 options(shiny.maxRequestSize=50*1024^2)
-calc_prob <- function(cM){(exp(-3.5+0.64*cM)/(1+exp(-3.5+0.64*cM))) %>% round(2)}
+calc_prob <- function(cM){(exp(-5.86584+0.78623*cM)/(1+exp(-5.86584+0.78623*cM))) %>% round(2)}
 
 shinyServer(function(input, output, session) {
 
@@ -19,11 +19,12 @@ shinyServer(function(input, output, session) {
     } else {
       out <- inFile()$datapath %>% read_csv %>% 
         unique() %>% 
-        mutate(new_cm = CENTIMORGANS*calc_prob(CENTIMORGANS)) %>% 
+        mutate(new_cm = CENTIMORGANS*calc_prob(CENTIMORGANS)) %>% filter(CHROMOSOME != "X")%>% 
         group_by(MATCHNAME) %>% 
         summarise(`UNWEIGHTED SUM OF CENTIMORGANS` = sum(CENTIMORGANS) %>% round(2),
                   `REWEIGHTED SUM OF CENTIMORGANS` = (sum(new_cm)-max(new_cm)+max(CENTIMORGANS)) %>% round(2),
-                  `LONGEST SEGMENT` = max(CENTIMORGANS) %>% round(2)) %>% ungroup %>% arrange(desc(`REWEIGHTED SUM OF CENTIMORGANS`))
+                  `LONGEST SEGMENT` = max(CENTIMORGANS) %>% round(2)) %>% ungroup %>% arrange(desc(`REWEIGHTED SUM OF CENTIMORGANS`)) %>% 
+        ungroup() %>% mutate(SCALING = (`REWEIGHTED SUM OF CENTIMORGANS`/`UNWEIGHTED SUM OF CENTIMORGANS`) %>% round(3))
       out}
   })
   
