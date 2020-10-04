cd1b57be424760f1352340f73c4dabd6ea473483
Staffan Betnér <staffan@betner.nu>
2020-10-04 16:43:11 +0200

    Progress spinner

--- ui.R
+++ ui.R
@@ -1,13 +1,11 @@
-library(shiny)
-library(htmlwidgets)
-library(shinydashboard)
-library(DT)
+pacman::p_load(shiny, htmlwidgets, shinydashboard, DT, shinycssloaders)
 dashboardPage(
   dashboardHeader(title = "Reweighter of Family Finder Matches List", titleWidth = 400),
   dashboardSidebar(fileInput('file', HTML("Upload one CSV File with<br />Chromosome Browser Results<br />(Max. Size 50 MB)"),
                              accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'), multiple = F, width = '95%'),
                    checkboxInput("anonymize", "Hide names of matches", value = FALSE),
                    helpText("The tool weights each segment by the probability that it is a identical by descent (IBD) segment. The segments are summed up for each match name, so several matches with the same name will be combined." ,
+                            "If any segments within a match name overlaps with another segment in the same name set, \"overlapping segments within name\" will be set as \"true\"",
                            # "However, if you also upload a matches list, then the segments will be allocated so that the sum will check out. There might be errors in smaller segment however.",
                             br(),
                            br(),
@@ -18,4 +16,4 @@ dashboardPage(
                             tags$p("Source available",tags$a(href = "https://github.com/StaffanBetner/ffreweighter/", "here.")),br(),
                             "Contact: staffan@betner.nu or @StaffanBetner (Twitter)"),
                    width = 350),
-  dashboardBody(DTOutput("table")))
+  dashboardBody(DTOutput("table") %>% withSpinner()))
